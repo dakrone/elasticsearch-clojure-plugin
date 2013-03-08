e186f82641e8f466b3e655e7b2108d9ae9b463e9
Lee Hinman <lee@writequit.org>
2013-03-08 14:05:48 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/dakrone/elasticsearch-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-beta10"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.1.2"]
-                 [org.elasticsearch/elasticsearch "0.20.4"]]
+                 [org.elasticsearch/elasticsearch "0.20.5"]]
   :aot [esclojure.ClojureHandler
         esclojure.ClojureModule
         esclojure.action.ClojureFibonacciRequest
